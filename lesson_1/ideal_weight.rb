889379558671022c59398696fe6dda9e7c24706c
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-08-24 20:54:00 +0300

    Add to_f

--- lesson_1/ideal_weight.rb
+++ lesson_1/ideal_weight.rb
@@ -1,10 +1,10 @@
 puts "Введите имя"
 name = gets.chomp
 puts "Введите рост"
-growth =gets.chomp
+growth =gets.chomp.to_f
 
 if (growth - 110) * 1.15 > 0
-  puts "#{name}, Ваш результат #{growth}."
+  puts "#{name}, Ваш результат #{(growth - 110) * 1.15}."
 else
   puts "#{name}, Ваш вес уже оптимальный."
 end
