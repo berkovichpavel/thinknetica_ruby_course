182f84ad697eec8b6153ddb1070de6d439eb6407
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-08-24 21:28:55 +0300

    Download rubocop

--- lesson_1/area_of_a_triangle.rb
+++ lesson_1/area_of_a_triangle.rb
@@ -1,6 +1,6 @@
-puts "Введите основание треугольника"
+puts 'Введите основание треугольника'
 base = gets.chomp.to_f
-puts "Введите высту"
-height =gets.chomp.to_f
+puts 'Введите высту'
+height = gets.chomp.to_f
 
-puts "Площадь треугольника = #{(0.5*base*height).round(3)}."
+puts "Площадь треугольника = #{(0.5 * base * height).round(3)}."
--- lesson_1/ideal_weight.rb
+++ lesson_1/ideal_weight.rb
@@ -1,9 +1,9 @@
-puts "Введите имя"
+puts 'Введите имя'
 name = gets.chomp
-puts "Введите рост"
-growth =gets.chomp.to_f
+puts 'Введите рост'
+growth = gets.chomp.to_f
 
-if (growth - 110) * 1.15 > 0
+if ((growth - 110) * 1.15).positive?
   puts "#{name}, Ваш результат #{(growth - 110) * 1.15}."
 else
   puts "#{name}, Ваш вес уже оптимальный."
