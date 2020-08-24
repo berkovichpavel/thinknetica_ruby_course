9629e38ad63a71be4149b2db2eba9197b349351c
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-08-24 21:05:57 +0300

    Add to_f

--- lesson_1/quadratic_equation.rb
+++ lesson_1/quadratic_equation.rb
@@ -8,14 +8,14 @@ c = gets.chomp.to_f
 
 d = (b**2) - (4 * a * c)
 
-if d > 0
+if d.positive?
   d_sqrt = Math.sqrt(d)
-  x1 = (-b + d_sqrt)/(2 * a)
-  x2 = (-b - d_sqrt)/(2 * a)
+  x1 = (-b + d_sqrt) / (2 * a)
+  x2 = (-b - d_sqrt) / (2 * a)
   puts "Дискриминант = #{d}, x1 = #{x1}, x2 = #{x2}"
-elsif d == 0
-  x = -b/(2 * a)
+elsif d.zero?
+  x = -b / (2 * a)
   puts "Дискриминант = #{d}, корень = #{x}"
-elsif d < 0
+elsif d.negative?
   puts 'Нет рациональных корней'
 end
