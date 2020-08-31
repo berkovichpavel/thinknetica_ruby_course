23e184cc6b743aea8f585c39f8fdd4dfdd352476
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-08-31 12:45:58 +0300

    Add my 5 homework

--- lesson_5/lib/instance_counter.rb
+++ lesson_5/lib/instance_counter.rb
@@ -1,7 +1,7 @@
 module InstanceCounter
   def self.included(base)
     base.extend ClassMethods
-    base.send :include, InstanceMethods
+    base.include, InstanceMethods
   end
 
   module ClassMethods
