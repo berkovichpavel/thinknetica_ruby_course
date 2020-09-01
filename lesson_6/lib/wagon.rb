879987e61e35463fb2ac7da0f6cf50e23c1ed02c
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-09-01 14:50:11 +0300

    initial commit

---
+++ lesson_6/lib/wagon.rb
@@ -0,0 +1,21 @@
+require_relative 'company_name'
+require_relative 'instance_counter'
+
+class Wagon
+  include CompanyName
+  include InstanceCounter
+  attr_accessor :type, :number
+  @@serial_number = 1
+
+  def initialize
+    @number = @@serial_number
+    @@serial_number += 1
+    register_instance
+    add_company_name
+  end
+
+  def add_company_name
+    print 'Add company name to Wagon: '
+    self.company_name = STDIN.gets.chomp
+  end
+end
